7857ca80e2734529fe6f27789f91cb6e3ad8c8f6
BoringMan60101 <80347868+BoringMan60101@users.noreply.github.com>
2022-12-30 10:45:18 +0500

    Delete version1 directory

--- version1/solver.hpp
+++
@@ -1,9 +0,0 @@
-//В дальнейшем возможности могут быть расширены. Например коэффициенты могут перестать юыть константами.
-//double * TDMA(double *T0, struct Q mesh, struct constPhysProperties consts, unsigned t); //Возможно на будущее
-#ifndef __CV_SOLVER__
-#define __CV_SOLVER__
-#include "my_types.hpp"
-void checkMemAlloc(void *pointer, const char *ErrMsg);
-double ** getExactSolution(const discrMesh &mesh);
-double * TDMA(double *T0, const discrMesh &mesh, const physConsts &constsm, uns t);
-#endif
